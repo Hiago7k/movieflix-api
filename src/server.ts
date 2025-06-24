9fa98ebc310a228604fe22be299af07d9f57e541
Hiago 7k <hiago7k@gmail.com>
2025-06-24 19:11:00 -0300

    adicionada ordenacao, genero do filme

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,17 @@ const app = express();
 const prisma = new PrismaClient
 
 
-app.get("/movies", async (req, res) => {
-    const movies = await prisma.movie.findMany(); // metodo para achar os filmes do banco de dados
-    res.json(movies); // retorno da nossa api vai ser um json com todos os filmes
+app.get("/movies", async (_, res) => {
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: "asc"
+        },
+        include: {
+            genres: true,
+            languages: true
+        }
+    });   
+    res.json(movies); 
 })
 
 app.listen(port, () => {
