61747cfae5f5a5c653cdff9ac003313c2d4ce40e
Hiago 7k <hiago7k@gmail.com>
2025-07-14 19:05:46 -0300

    adicionado filtro de filmes por genero

--- src/server.ts
+++ src/server.ts
@@ -110,6 +110,30 @@ app.delete("/movies/:id", async (req, res) => {
 
 })
 
+app.get("/movies/:genreName", async (req, res) => {
+    try {
+        const moviesFilteredByGenreName = await prisma.movie.findMany({
+            include: {
+                genres: true,
+             //  languages: true,
+            },
+            where: {
+                genres: {
+                    name: {
+                        equals: req.params.genreName,
+                        mode: "insensitive",
+                    },
+                },
+            },
+        });
+
+        res.status(200).send(moviesFilteredByGenreName);
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao atualizar um filme" });
+    }
+
+});
+
 app.listen(port, () => {
     console.log(`Servidor em execucao na porta ${port}`);
 });
